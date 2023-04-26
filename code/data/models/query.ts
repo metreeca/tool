34071982ad8a041913152468aa6a3eb98b3d8d6f
Alessandro Bollini <alessandro.bollini@metreeca.com>
2023-04-26 10:44:51 +0200

    Data / useQuery() / Add query persistence

--- code/data/models/query.ts
+++ code/data/models/query.ts
@@ -14,36 +14,73 @@
  * limitations under the License.
  */
 
-import { asFrame, Frame } from "@metreeca/core/entry";
-import { useRoute }       from "@metreeca/data/contexts/router";
-import { Setter }         from "@metreeca/data/hooks";
-import { useState }       from "react";
+import { isEmpty }             from "@metreeca/core";
+import { asFrame, Frame }      from "@metreeca/core/entry";
+import { useRouter }           from "@metreeca/data/contexts/router";
+import { Setter }              from "@metreeca/data/hooks";
+import { useEffect, useState } from "react";
 
+/**
+ * Persistent query storage.
+ *
+ * @module
+ */
 
+/**
+ * Creates a persistent query store.
+ */
 export function useQuery(): [Frame, Setter<Frame>] {
 
-	const [route]=useRoute();
+	const [route]=useRouter();
 
-	const [query, setQuery]=useState(asFrame(history.state) ?? {});
+	const key=`${route}#query`;
 
-	function setStore(query: Frame) {
-		history.replaceState(query, document.title);
-	}
+	const value=asFrame(history.state)
+		?? decode(location.search.substring(1))
+		?? decode(sessionStorage.getItem(key))
+		?? {};
 
-	return [query, query => {
-		setStore(query);
+	const [query, setQuery]=useState(value);
+
+	useEffect(() => { update(value); }, []);
+
+	return [query, update];
+
+
+	function update(query: Frame) {
+
+		const search=encode(query);
+
+		history.replaceState(query, document.title,
+			search ? `${location.pathname}?${search}${location.hash}` : `${location.pathname}${location.hash}`
+		);
+
+		sessionStorage.setItem(key, search);
+
 		setQuery(query);
-	}];
+	}
 
 }
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+// !!! compact encoding / handle versioning
+
 function encode(query: Frame): string {
-	return encodeURI(JSON.stringify(query));
+	return isEmpty(query) ? "" : encodeURI(JSON.stringify(query));
 }
 
-function decode(query: string): Frame {
-	return JSON.parse(decodeURI(query) || "{}"); // !!! handle errors
+function decode(query: undefined | null | string): undefined | Frame {
+	try {
+
+		return query ? JSON.parse(decodeURI(query)) : undefined;
+
+	} catch ( e ) {
+
+		console.warn("malformed query sting <%o> / %o", query, e);
+
+		return undefined;
+
+	}
 }
